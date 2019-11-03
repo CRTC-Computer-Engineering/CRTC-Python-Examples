a2115148de7938da0f3aa4ab445f7e63a940e173
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-11-03 17:44:07 -0500

    framework for adventure game

---
+++ Basic Text Adventure Game/Adventure.py
@@ -0,0 +1,67 @@
+import yaml
+import Levenshtein as lev
+
+select_edits = 2 # How close an input has to be to match
+current_yaml = "gamedata/lobby.yaml"
+
+selected_direction = ''
+
+def enter_room(filepath):
+    """Reads data from a yaml file"""
+    with open(filepath, "r") as file_desc:
+        data = yaml.full_load(file_desc)
+    return data
+
+def save_data(filepath, data):
+    """Dumps data to a yaml"""
+    with open(filepath, "w") as file_desc:
+        yaml.dump(data, file_desc)
+
+if __name__ == "__main__":
+    current_room = enter_room(current_yaml)
+    print (current_room['roommeta']['desc'])
+    while(True):
+    
+        user_command = (input(":")).upper()
+        user_arg_1 = user_command.split()[0]
+        try:
+            user_arg_2 = user_command.split()[1]
+        except:
+            user_arg_2 = ""
+        
+        if (lev.distance(user_arg_1, "LOOK")) < select_edits:
+            if (lev.distance(user_arg_2, "LEFT")) < select_edits:
+                selected_direction = 'left'
+                print(current_room['left']['desc'])
+            elif (lev.distance(user_arg_2, "RIGHT")) < select_edits:
+                selected_direction = 'right'
+                print(current_room['right']['desc'])
+            elif (lev.distance(user_arg_2, "AHEAD")) < select_edits or (lev.distance(user_arg_1, "FORWARD")) < select_edits:
+                selected_direction = 'ahead'
+                print(current_room['ahead']['desc'])
+            elif (lev.distance(user_arg_2, "BEHIND")) < select_edits or (lev.distance(user_arg_1, "BACK")) < select_edits:
+                selected_direction = 'behind'
+                print(current_room['behind']['desc'])
+        elif (lev.distance(user_arg_1, "INSPECT")) < select_edits or (lev.distance(user_arg_1, "EXAMINE")) < select_edits:
+            print(current_room[selected_direction][user_arg_2.lower()]['inspect'])
+        elif (lev.distance(user_arg_1, "PICKUP")) < select_edits or (lev.distance(user_arg_1, "GRAB")) < select_edits:
+            print("pickup")
+        elif (lev.distance(user_arg_1, "OPEN")) < select_edits:
+            if user_arg_2 == "DOOR":
+                destination = current_room[selected_direction][user_arg_2.lower()]['dest']
+                if destination != "Nowhere":
+                    confirm = input(destination + ", would you like to enter? Y/N: ")
+                    if confirm == "Y":
+                        current_yaml = current_room[selected_direction][user_arg_2.lower()]['path']
+                        current_room = enter_room(current_yaml)
+                        print (current_room['roommeta']['desc'])
+                    elif confirm == "N":
+                        break
+                    else:
+                        print("I did not understand the command")
+                else:
+                    print("This door does not lead anywhere, its a wonder somebody took the time to install it.")
+            else:
+                print("This is not a door")
+        else:
+            print("I did not understand")
