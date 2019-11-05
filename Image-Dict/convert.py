3f77663d117900375e5c2824d1d15c850edfe447
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-11-04 21:35:13 -0500

    This stuff works bby

--- Image-Dict/convert.py
+++ Image-Dict/convert.py
@@ -5,14 +5,12 @@ pix = selected_image.load()
 width, height = selected_image.size
 output_file = open("output.txt", "a")
 output_file.write("[")
-for row in range(height):
-    for column in range(width):
+for row in range(0, height, 4):
+    for column in range(0, width, 4):
         red, green, blue, alpha = pix[column, row]
-        print (str(red) + str(green) + str(blue))
+        print (str(red) + " " + str(green) + " " + str(blue))
         
-        if blue > 0:
+        if blue > 0 and green < blue and red < blue:
             output_color = "Blue"
-        else:
-            output_color = "White"
-        output_file.write("\"" + str(column) + " " + str(row) + " " + output_color + "\", ")
+            output_file.write("\"" + str(column) + " " + str(row) + " " + output_color + "\", ")
 output_file.write("]")
