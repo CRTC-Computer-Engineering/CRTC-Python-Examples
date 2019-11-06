7d99dd64f514a7d224f9d05a7a49b70872937332
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-11-06 10:19:50 -0500

    Here aidan, this will convert a bitmap for u <3

--- Image-Dict/convert.py
+++ Image-Dict/convert.py
@@ -5,13 +5,18 @@ pix = selected_image.load()
 width, height = selected_image.size
 output_file = open("output.txt", "a")
 output_file.write("[")
-for row in range(0, height, 1):
-    for column in range(0, width, 1):
+resolution = 2
+
+for row in range(0, height, resolution):
+    for column in range(0, width, resolution):
         red, green, blue, alpha = pix[column, row]
         print (str(red) + " " + str(green) + " " + str(blue))
         
         #if blue > 0 and green < blue and red < blue:
-        if blue < 30 and green < 30 and red < 30:
-            output_color = "Blue"
+        if blue < 100 and green > 30 and red < 100:
+            output_color = "Green"
+            output_file.write("\"" + str(column) + " " + str(row) + " " + output_color + "\", ")
+        elif blue < 30 and green > 30 and red > 30:
+            output_color = "Yellow"
             output_file.write("\"" + str(column) + " " + str(row) + " " + output_color + "\", ")
 output_file.write("]")
